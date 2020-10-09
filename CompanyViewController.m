f0644a8537b6a5425b312b63443cf01a7606aedd
iqbal92 <ikbal.pixster@gmail.com>
2020-10-09 12:59:42 +0530

    Update CompanyViewController.m

--- CompanyViewController.m
+++ CompanyViewController.m
@@ -33,6 +33,34 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+/*
+    func setNotificationEndFast(identifire : Int) {
+        
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["sdfsdfds"])
+        
+        let content = UNMutableNotificationContent()
+        content.title = "sdfd"
+        content.categoryIdentifier = "sdfsdf"
+        content.body = "sdfsdfsdf"
+        content.sound = UNNotificationSound.default
+        
+        let triggerDateComponent = Calendar.current.dateComponents([.day, .hour, .minute], from: Date().addHours(identifire))
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDateComponent, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "fast end", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
+           if error != nil {
+               print(error!)
+               
+           } else {
+               print("\(request.description)")
+           }
+       })
+    }
+*/
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
